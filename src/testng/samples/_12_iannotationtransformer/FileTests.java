1fcafcb3c732a73944567f5b6c7286a49e72729f
Green, Tal (tg4920) <tg4920@intl.att.com>
2018-04-10 09:19:23 +0300

    add annotation transformer example

---
+++ src/testng/samples/_12_iannotationtransformer/FileTests.java
@@ -0,0 +1,45 @@
+package testng.samples._12_iannotationtransformer;
+
+import org.testng.SkipException;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+public class FileTests
+{
+
+	@BeforeClass
+	public void init()
+	{
+		System.out.println("\n\t In FileTests1 - Init \n");
+	}
+	
+	@Test
+	private void test1()
+	{
+		System.out.println("\t In FileTests1 - Test 1");
+		throw new SkipException("sdf");
+	}
+	
+	
+	@Test
+	private void test3()
+	{
+		System.out.println("\t In FileTests1 - Test 3");
+	}
+	
+	@Test
+	private void test2()
+	{
+		System.out.println("\t In FileTests1 - Test 2");
+	}
+	
+	@AfterClass
+	public void teardown()
+	{
+		System.out.println("\n\t In FileTests1 - teardown \n\n");
+	}
+	
+	
+}
---
+++ src/testng/samples/_12_iannotationtransformer/Transformer.java
@@ -0,0 +1,30 @@
+package testng.samples._12_iannotationtransformer;
+
+import org.testng.IAnnotationTransformer;
+import org.testng.annotations.ITestAnnotation;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+public class Transformer implements IAnnotationTransformer {
+    // Do not worry about calling this method as testNG calls it behind the scenes before EVERY method (or test).
+    // It will disable single tests, not the entire suite like SkipException
+    @Override
+    public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
+
+        // If we have chose not to run this test then disable it.
+        if (disableMe(testMethod)) {
+            annotation.setEnabled(false);
+        }
+    }
+
+    // logic YOU control
+    private boolean disableMe(Method testMethod) {
+        if (testMethod.getName().equals("test2")) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
